d68df0a536c99be2ffa2ee8abba0ec031c922d63
Bangaly KAKE <bangaly.kake@gmail.com>
2017-05-31 15:12:37 +0200

    Initialize project

---
+++ src/main/java/fr/free/bkake/springjooq/SpringJooqApplication.java
@@ -0,0 +1,12 @@
+package fr.free.bkake.springjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJooqApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/free/bkake/springjooq/SpringJooqApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.free.bkake.springjooq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJooqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
